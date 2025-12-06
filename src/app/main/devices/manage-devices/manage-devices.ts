ca2ccdec19ab38fcb487eccfb5075749e48961eb
Joel Riba <joel.irungu00@gmail.com>
2025-12-06 03:45:05 +0300

    With saving visitorId feature

--- src/app/main/devices/manage-devices/manage-devices.ts
+++ src/app/main/devices/manage-devices/manage-devices.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { MainService } from './../../main-service';
 
 @Component({
   selector: 'app-manage-devices',
   imports: [],
-  templateUrl: './manage-devices.html',
+
+templateUrl: './manage-devices.html',
   styleUrl: './manage-devices.css'
 })
-export class ManageDevices {
+export class ManageDevices implements OnInit {
 
+
+  constructor(private mainService :MainService) { }
+
+  ngOnInit():void{
+      this.existingDeviceCheck();
+  }
+
+  existingDeviceCheck() {
+    const userId = localStorage.getItem('userId') || '';
+    if (!userId) return;
+
+    this.mainService.getUserById(userId).subscribe({
+      next: (data: any) => {
+        console.log('User data fetched for device management:', data);
+      },
+      error: (error: any) => {
+        console.error('Error fetching user data:', error);
+      },
+      complete: () => {
+        console.log('Completed fetching existing devices');
+      }
+    });
+  }
 }
