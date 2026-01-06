0cec9a728f9183c1d765d6e4cad61522a20891e4
Joel Riba <joel.irungu00@gmail.com>
2026-01-06 18:06:16 +0300

    Leaflet asset path fix

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-import * as L from 'leaflet';
-delete (L.Icon.Default.prototype as any)._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: '/leaflet/marker-icon-2x.png',
-  iconUrl: '/leaflet/marker-icon.png',
-  shadowUrl: '/leaflet/marker-shadow.png',
-});
 
 bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+  .catch((err:any) => console.error(err));
