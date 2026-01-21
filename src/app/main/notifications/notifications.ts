8af1e7a4e2f36aa10e3e7de23cfca8ceeb4c1f05
Joel Riba <joel.irungu00@gmail.com>
2026-01-21 16:12:40 +0300

    Fix the double pin entry

--- src/app/main/notifications/notifications.ts
+++ src/app/main/notifications/notifications.ts
@@ -2,18 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainService } from '../main-service';
 import { User } from '../interfaces/user';
-import { Devices } from '../devices/devices';
+
 import { Location } from '../interfaces/location';
 import { LocationService } from './../../map/location-service';
 import { Router } from '@angular/router';
 import {Socket, io} from 'socket.io-client';
-
+import { NgOtpInputComponent } from 'ng-otp-input';
+import { LatestLocation } from './../interfaces/latest-location';
 
 
 @Component({
   selector: 'app-notifications',
-  imports: [CommonModule],
-  templateUrl: './notifications.html',
+  imports: [CommonModule, NgOtpInputComponent],
+
+templateUrl: './notifications.html',
   styleUrl: './notifications.css'
 })
 export class Notifications implements OnInit{
@@ -21,30 +23,47 @@ export class Notifications implements OnInit{
   showNotification=false;
   notificationTimer: any;
   socket!: Socket;
-
   latestNotification: any=[];
-
   latestLocation:any | null=null;
 userLatestLocation:Location | null=null;
   locationPic='/location.png';
   trackingDevices:any[]=[];
   profilePic = '/user.png';
   pin: string = '';
-  pinBoxes: string[] = ['', '', '', '', '', ''];
   expandProfile: boolean = false;
   numberOfDevices: Number = 0;
   user!: User;
   location: any;
 sharePic='/share.png';
+
 showTrackSection = false;
   constructor(private mainService: MainService, private locationService: LocationService, private router:Router) {}
  
-
+  otpConfig = {
+    length: 6,
+    allowNumbersOnly: true,
+    inputStyles: {
+      width: '2.7rem',
+      height: '2.7rem',
+      color: 'white',
+      background: 'transparent',
+      border: '2px solid rgba(75, 167, 22, 0.5)',
+      borderRadius: '0.75rem',
+      fontSize: '1.6rem',
+      fontWeight: '500',
+      transition: 'all 0.25s ease'
+    }
+    
+  };
+  
+ onOtpChange(otp: string) {
+  this.pin = otp;
+  console.log('OTP entered:', this.pin);
+}
+  
   ngOnInit(): void {
     this.receivedNotification()
     const userId = localStorage.getItem('userId')!; // ! used since The user id MUST be there after signin and that it will be passed
-
-    this.onGetDashInformation(userId);
 
     this.locationService.watchLocationOnInit(userId)
   
@@ -52,92 +71,29 @@ showTrackSection = false;
 
 
   
-  onGetDashInformation(_id: string): void {
-    this.mainService.getUserById(_id).subscribe(
-      (response: any) => {
-        this.user = response.user;
-        if (response.LatestLocation && response.LatestLocation.length > 0) {
-          const latest = response.LatestLocation[0];
-          this.latestLocation = `Lat: ${latest.latitude}, Lon: ${latest.longitude}`;
-        } else {
-          this.latestLocation = 'No location available';
-        }
-      },
-
-      (error: any) => {
-        console.log(error);
-      },
-      () => console.log('complete')
-    );
-  }
-  showPinInput = false;
-
-  moveNext(event: any, index: number) {
-  const input = event.target as HTMLInputElement;
-  const next = input.nextElementSibling as HTMLInputElement | null;
-  if (input.value && next) {
-    next.focus();
-  }
-}
-
-movePrev(event: KeyboardEvent) {
-  const input = event.target as HTMLInputElement;
-  const prev = input.previousElementSibling as HTMLInputElement | null;
-  if (event.key === 'Backspace' && !input.value && prev) {
-    prev.focus();
-  }
-}
-
-
-  updatePin(event: any, index: number) {
-    const value = event.target.value;
-      this.pinBoxes[index] = value;
-    this.pin = this.pinBoxes.join('');
-    console.log('PIN:', this.pin);
   
-    const next = event.target.nextElementSibling as HTMLInputElement | null;
-    if (value && next) next.focus();
-  }
-
-  onEnterPin(){
+  onEnterPin() {
     if (this.pin.length !== 6) {
       console.warn('Please enter a valid 6-digit PIN');
-    
       return;
     }
-  this.mainService.getUserDataByPin(this.pin).subscribe({
-
-    next: (user:any)=>{
-
-      if (!user.User || !user.User) {
-        console.warn('No user found for this PIN');
-        return;
-      }
-      const existingUser = this.trackingDevices.some(
-        (alreadyAddedUser: any) => alreadyAddedUser.User._id === user.User._id
-      );
-      if (existingUser) {
-        return;
-        
-      } else {
-        this.trackingDevices.push(user);
-        const userLatestLocation = user.LatestLocation || null;
-        localStorage.setItem('trackingDevices', JSON.stringify(this.trackingDevices));
-      }
-          },
-   error: (error:any)=>{
-    console.log(error)
-
   
-  },
-  complete: () =>{
-     console.log('Request complete')
-     
-    }
+    this.mainService.getUserDataByPin(this.pin).subscribe({
+      next: (user: any) => {
+        if (!user?.User) {
+          console.warn('No user found for this PIN');
+          return;
+        }
+        this.trackingDevices = [user];
+        console.log('Tracking device added:', user);
+        this.userLatestLocation=user.LatestLocation;
+        console.log('User latest location:', this.userLatestLocation);
 
-})
- 
-}
+      },
+      error: (err:any) => console.error(err),
+      complete: () => console.log('Tracking device replaced')
+    });
+  }
 
 openAdvancedTracking(){
   this.showTrackSection = !this.showTrackSection;
