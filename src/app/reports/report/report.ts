f88c0bbe739e3532df14523f50fd0d93ccf31c26
Joel Riba <joel.irungu00@gmail.com>
2026-02-01 21:30:52 +0300

    add sign in

--- src/app/reports/report/report.ts
+++ src/app/reports/report/report.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { BackButton } from '../../shared/back-button/back-button';
 
 @Component({
   selector: 'app-report',
-  imports: [],
+  imports: [BackButton],
   templateUrl: './report.html',
   styleUrl: './report.css'
 })
